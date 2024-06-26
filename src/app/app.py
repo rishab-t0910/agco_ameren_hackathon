1171e155f968b0f8def93a334866b7454d358ae5
atharvanaik <atharvanaik10@gmail.com>
2024-06-26 12:59:59 -0500

    Add database handler

--- src/app/app.py
+++ src/app/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from database_handler import DatabaseHandler
 
 app = Flask(__name__)
+CORS(app)
+
+db = DatabaseHandler()
 
 @app.route('/count', methods=['GET'])
 def get_count():
---
+++ src/database_handler.py
@@ -0,0 +1,28 @@
+from google.cloud import firestore
+from dotenv import load_dotenv
+import os
+
+class DatabaseHandler:
+
+    def __init__(self):
+        # Load environment
+        load_dotenv()
+        credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+        # Check if the credentials environment variable is set
+        if credentials_path:
+            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+        else:
+            raise EnvironmentError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set.")
+
+        # Initialize firestore
+        self.db = firestore.Client()
+        self.collection_name = 'streaming_data'
+
+    def verify_schema(data):
+        return False
+
+    def add_data(self, data):
+        self.db.collection(self.collection_name).add(data)
+
+
+
