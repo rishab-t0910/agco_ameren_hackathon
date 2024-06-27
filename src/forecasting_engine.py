172f08f01c4ff0d6487bf5e65eec576ecd361e9d
Rishab Tirupathi <rishab.t0910@gmail.com>
2024-06-26 20:34:39 -0500

    Updated forecasting_engine.py

--- src/forecasting_engine.py
+++ src/forecasting_engine.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import os
 from dotenv import load_dotenv
+from flask import jsonify
 
 node_id = 1
 load_dotenv("../.env")
@@ -10,6 +11,19 @@ url = os.getenv('API_URL')
 
 def get_data(start=None, end=None):
     if not start and not end:
-        response = requests.get(url)
+        response = requests.get(url + "/data")
     elif not start:
-        response = requests.get()
+        response = requests.get(url + f"/data?node_id=1&start={start}")
+    elif not end:
+        response = requests.get(url + f"/data?node_id=1&end={end}")
+    else:
+        response = requests.get(url + f"/data?node_id=1&start={start}&end={end}")
+
+    json_file = response.json()
+    pd_df = pd.DataFrame(json_file)
+
+    return pd_df
+
+def write_forecast(df, node_id = 1):
+    json_str = df.to_json(orient='records')
+    return requests.post(url + f"/forecast?node_id=1", jsonify(json_str))
