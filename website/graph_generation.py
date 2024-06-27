50be368f7335b333e8902e3dc43c16b9f47db1cb
clarso5 <104173067+clarso5@users.noreply.github.com>
2024-06-26 21:55:36 -0500

    basic graph generation script written

--- website/graph_generation.py
+++ website/graph_generation.py
@@ -2,13 +2,22 @@
 # the occupancy of grainger library from the past 6 hours as well as a predictive forcast 
 # of the occupancy for the next 6 hours in the future.
 
-import matplotlib
+import matplotlib.pyplot as plt
 import requests
 import pandas as pd
 #import jsonify
 node_id = 1
-url = "http://192.168.71.126:5050"
+url = "http://occupi-rp-hack-24.uc.r.appspot.com"
 
-response = requests.get(url+ "/data?node_id="+ str(node_id))
+response = requests.get(url+ "/data?node_id="+ str(node_id)).json()
+response_df = pd.json_normalize(response, "data")
+print(response_df)
+last_row = response_df.iloc[-1]["timestamp"]
+plt.plot(response_df["timestamp"], response_df["count"]) 
+plt.xlabel("Time")
+plt.ylabel("Occupancy")
+plt.xticks(last_row)
+plt.show()
+plt.savefig('plot.png')
 
-print(response.json())
+#print(response)
